5266a5cebd7738814e0b85475caeba327f3f8cba
Max <asipomars@gmail.com>
2019-04-04 19:05:40 +0300

    Add database scripts for visibility. #TEL-18 To Verify

---
+++ app/models/dbfunctions/visibility.js
@@ -0,0 +1,70 @@
+const db = require(__dirname + '/../db.js').db
+
+async function visibility() {
+	try {
+		var data = await db.func('Visibility')
+		return data
+	}
+	catch(err) {
+		throw err
+	}
+}
+
+async function sortVisibility() {
+	try {
+		var data = await db.func('SortVisibility')
+		return data
+	}
+	catch(err) {
+		throw err
+	}
+}
+
+async function telescopeVisibility(searchtext) {
+	try {
+		var data = await db.func('telescopeVisibility', searchtext)
+		return data
+	}
+	catch(err) {
+		throw err
+	}
+}
+
+async function objectVisibility(searchtext) {
+	try {
+		var data = await db.func('ObjectVisibility', searchtext)
+		return data
+	}
+	catch(err) {
+		throw err
+	}
+}
+
+async function createVisibility(ids) {
+	try {
+		var data = await db.func('CreateVisibility', ids)
+		return data
+	}
+	catch(err) {
+		throw err
+	}
+}
+
+async function deleteVisibilityByID(id) {
+	try {
+		var data = await db.func('DeleteVisibilityByID', id)
+		return data
+	}
+	catch(err) {
+		throw err
+	}
+}
+
+module.exports = {
+	visibility : visibility,
+	sortVisibility : sortVisibility,
+	telescopeVisibility : telescopeVisibility,
+	objectVisibility : objectVisibility,
+	createVisibility : createVisibility,
+	deleteVisibilityByID : deleteVisibilityByID
+}
