053a6dcf7d9b41a2334439ef3f95a9db8546be77
Max <asipomars@gmail.com>
2019-04-04 16:19:33 +0300

    Add database scripts for objects. #TEL-17 To Verify Revision

---
+++ app/models/dbfunctions/objects.js
@@ -0,0 +1,89 @@
+const db = require(__dirname + '/../db.js').db
+
+async function objects() {
+	try {
+		var data = await db.func('Objects')
+		return data
+	}
+	catch(err) {
+		throw err
+	}
+}
+
+async function sortObjects() {
+	try {
+		var data = await db.func('SortObjects')
+		return data
+	}
+	catch(err) {
+		throw err
+	}
+}
+
+async function searchObjects(searchtype, searchtext) {
+	try {
+		var data = await db.func('SearchObjects', [searchtype, searchtext])
+		return data
+	}
+	catch(err) {
+		throw err
+	}
+}
+
+async function createObject(objname, objh, obja, objinfo) {
+	try {
+		await db.func('CreateObject', [
+			objname,
+			objh,
+			obja,
+			objinfo
+		])
+	}
+	catch(err) {
+		throw err
+	}
+}
+
+async function objectByID(id) {
+	try {
+		var data = await db.func('ObjectByID', id)
+		return data
+	}
+	catch(err) {
+		throw err
+	}
+}
+
+async function changeObject(id, objname, objh, obja, objinfo) {
+	try {
+		await db.func('ChangeObject', [
+			id,
+			objname,
+			objh,
+			obja,
+			objinfo
+		])
+	}
+	catch(err) {
+		throw err
+	}
+}
+
+async function deleteObjectByID(id) {
+	try {
+		await db.func('DeleteObjectByID', id)
+	}
+	catch(err) {
+		throw err
+	}
+}
+
+module.exports = {
+	objects : objects,
+	sortObjects : sortObjects,
+	searchObjects : searchObjects,
+	createObject : createObject,
+	objectByID : objectByID,
+	changeObject : changeObject,
+	deleteObjectByID : deleteObjectByID
+}
