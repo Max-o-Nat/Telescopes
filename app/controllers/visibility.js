66989776eb6911a51de0fcb482f171d411b03a03
Natasha Zharova <zhar97@yandex.ru>
2019-04-14 22:34:47 +0300

    Add visibility documentation #TEL-27 To Verify

--- app/controllers/visibility.js
+++ app/controllers/visibility.js
@@ -2,6 +2,20 @@ var express = require('express')
 var router = express.Router()
 var db = require(__dirname + '/../models/dbfunctions/visibility')
 
+/**
+ * @api {get} /visibility/ Запрос информации о видимостях
+ * @apiName GetVisibility
+ * @apiGroup Visibility
+ *
+ * @apiSuccess {String} title Название страницы
+ * @apiSuccess {Object} login Только вошедшие в систему пользователи могут получить данную страницу
+ * @apiSuccess {String} login.username Имя пользователя
+ * @apiSuccess {String="admin","user"} login.role Роль пользователя
+ * @apiSuccess {Object[]} info Массив объектов с информацией о каждой вдимости
+ * @apiSuccess {Number} info.id Идентификатор видимости
+ * @apiSuccess {Number} info.telid Идентификатор телескопа
+ * @apiSuccess {Number} info.objid Идентификатор небесного тела
+ */
 router.get('/', async (req, res, next) => {
   try {
     var data = await db.visibility()
@@ -15,6 +29,22 @@ router.get('/', async (req, res, next) => {
   }
 })
 
+/**
+ * @api {post} /visibility/ Запрос сортированной информации о видимостях
+ * @apiName PostSortVisibility
+ * @apiGroup Visibility
+ *
+ * @apiParam {String="ID телескопа","ID небесного тела"} sorttype Признак, по которому происходит сортировка
+ *
+ * @apiSuccess {String} title Название страницы
+ * @apiSuccess {Object} login Только вошедшие в систему пользователи могут получить данную страницу
+ * @apiSuccess {String} login.username Имя пользователя
+ * @apiSuccess {String="admin","user"} login.role Роль пользователя
+ * @apiSuccess {Object[]} info Массив объектов с информацией о каждой вдимости
+ * @apiSuccess {Number} info.id Идентификатор видимости
+ * @apiSuccess {Number} info.telid Идентификатор телескопа
+ * @apiSuccess {Number} info.objid Идентификатор небесного тела
+ */
 router.post('/sort', async (req, res, next) => {
   try {
     var data = await db.sortVisibility()
@@ -28,6 +58,23 @@ router.post('/sort', async (req, res, next) => {
   }
 })
 
+/**
+ * @api {post} /visibility/ Запрос на поиск информации о видимостях
+ * @apiName PostSearchVisibility
+ * @apiGroup Visibility
+ *
+ * @apiParam {String="ID телескопа","ID небесного тела"} searchtype Признак, по которому происходит сортировка
+ * @apiParam {String} searchtext Текст поискового запроса
+  *
+ * @apiSuccess {String} title Название страницы
+ * @apiSuccess {Object} login Только вошедшие в систему пользователи могут получить данную страницу
+ * @apiSuccess {String} login.username Имя пользователя
+ * @apiSuccess {String="admin","user"} login.role Роль пользователя
+ * @apiSuccess {Object[]} info Массив объектов с информацией о каждой вдимости
+ * @apiSuccess {Number} info.id Идентификатор видимости
+ * @apiSuccess {Number} info.telid Идентификатор телескопа
+ * @apiSuccess {Number} info.objid Идентификатор небесного тела
+ */
 router.post('/search', async (req, res, next) => {
   try {
     var data = []
@@ -48,6 +95,16 @@ router.post('/search', async (req, res, next) => {
   }
 })
 
+/**
+ * @api {get} /visibility/ Запрос формы на создание видимости
+ * @apiName GetCreateVisibility
+ * @apiGroup Visibility
+ *
+ * @apiSuccess {String} title Название страницы
+ * @apiSuccess {Object} login Только вошедшие в систему пользователи могут получить данную страницу
+ * @apiSuccess {String} login.username Имя пользователя
+ * @apiSuccess {String="admin","user"} login.role Роль пользователя
+ */
 router.get('/create', [
   async (req, res, next) => {
     try {
@@ -62,6 +119,19 @@ router.get('/create', [
   }
 ])
 
+/**
+ * @api {post} /visibility/ Запрос на создание видимости
+ * @apiName PostCreateVisibility
+ * @apiGroup Visibility
+ *
+ * @apiParam {Number} info.telid Идентификатор телескопа
+ * @apiParam {Number} info.objid Идентификатор небесного тела
+ *
+ * @apiSuccess {String} title Название страницы
+ * @apiSuccess {Object} login Только вошедшие в систему пользователи могут получить данную страницу
+ * @apiSuccess {String} login.username Имя пользователя
+ * @apiSuccess {String="admin","user"} login.role Роль пользователя
+ */
 router.post('/create', [
   async (req, res, next) => {
     try {
@@ -86,6 +156,13 @@ router.post('/create', [
   }
 ])
 
+/**
+ * @api {get} /visibility/ Запрос на удаление видимости
+ * @apiName GetDeleteVisibility
+ * @apiGroup Visibility
+ *
+ * @apiParam {Number} info.id Идентификатор видимости
+ */
 router.get('/:id/delete', [
   async (req, res, next) => {
     try {
@@ -96,4 +173,5 @@ router.get('/:id/delete', [
     }
   }
 ])
+
 module.exports = router
